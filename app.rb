11632b22283cbf44635c4c292b92ab864bc6bf8d
Dana Simmons <dana@simmons-audio.com>
2019-02-19 03:06:29 +0000

    ran tests and finished code

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Dana"
+    end
+
+    get '/hometown' do
+        "My hometown is Jefferson, GA"
+    end
+
+    get '/favorite-song' do
+        'My favorite song is Lullaby'
+    end
 end
